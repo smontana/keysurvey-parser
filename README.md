ef715cd14b89e7c72531e5482b729a654c20939d
Stephen Montana <stephen.a.montana@gmail.com>
2016-03-25 16:07:00 -0500

    working parser

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # keysurvey-parser
-# keysurvey-parser
+
+##1-Month export
+1. Delete rows 1-5
+2. Clear text from new row 1
+3. Paste headers from corresponding file in **header\_files** folder to new row 1
+4. Save file to **survey\_files** folder
+5. Open **.env** file and set **FILE\_TO\_PARSE** equal to the name of the file (**including the .xls extension**) and set the **FIELDS\_TO\_USE\_FOR\_CSV** equal to which survey month it is (1, 3, 6, 12, or 18)
+6. npm start to run
+7. New CSV will appear in **parsed\_files** folder upon completion
---
+++ app.js
@@ -0,0 +1,121 @@
+require('dotenv').load();
+var express = require('express');
+var app = express();
+app.locals._ = require('lodash');
+var _ = require('lodash');
+app.locals.moment = require('moment');
+var moment = require('moment');
+
+// generate unique filename
+var unix_time = moment().unix().toString();
+var filename = './parsed_files/' + unix_time + '-' + process.env.FILE_TO_PARSE + '.csv';
+
+var fields_to_use_for_csv = process.env.FIELDS_TO_USE_FOR_CSV;
+
+// csv library
+var json2csv = require('json2csv');
+var fs = require('fs');
+var fields;
+
+if(fields_to_use_for_csv == 1) {
+  // Month 1 Fields
+  fields = ['response_number', 'submit_date', 'dt_id', 'lead_installer', 'phone', 'dealership', 'respondent_name', 'good_communication', 'good_communication_score', 'satisfaction_with_install', 'satisfaction_with_install_score', 'satisfied_with_training', 'satisfied_with_training_score', 'recommend_implementation_team', 'recommend_implementation_team_score', 'expectations_set', 'expectations_set_score', 'my_expectations_met', 'my_expectations_met_score', 'recommend_dt', 'recommend_dt_score', 'decision_making_yn', 'decision_making_yn_score', 'sales_based_met_expectations', 'sales_based_met_expectations_score', 'sales_satisfaction', 'sales_satisfaction_score', 'sales_rep_satisfaction', 'sales_rep_satisfaction_score', 'part_of_install', 'part_of_install_score', 'ease_of_use', 'ease_of_use_score', 'dms_effectiveness', 'dms_effectiveness_score', 'compelled', 'compelled_score', 'primary_role', 'primary_role_id', 'comments', 'unsatisfied_comments'];
+
+} else if(fields_to_use_for_csv == 3) {
+  // Month 3 Fields
+  fields = [];
+
+} else if(fields_to_use_for_csv == 6) {
+  // Month 6 Fields
+  fields = [];
+
+} else if(fields_to_use_for_csv == 12) {
+  // Month 12 Fields
+  fields = [];
+
+} else if(fields_to_use_for_csv == 18) {
+  // Month 18 Fields
+  fields = [];
+
+}
+
+// xls to json library
+var path_to_xls = './survey_files/' + process.env.FILE_TO_PARSE;
+
+var json_objs = [];
+
+if(typeof require !== 'undefined') XLS = require('xlsjs');
+var workbook = XLS.readFile(path_to_xls);
+var sheet = workbook.Sheets['Results'];
+
+var convert_to_json = XLS.utils.sheet_to_json(sheet);
+
+_.forEach(convert_to_json, function(a) {
+  // need to remove "amp;", "<font color=red>", and "<font color=green>"
+
+  var obj_string = JSON.stringify(a);
+  var prep1 = obj_string.replace(/<font color=red>/g, '');
+  var prep2 = prep1.replace(/<font color=green>/g, '');
+  var prep3 = prep2.replace(/amp;/g, '');
+
+  var b = JSON.parse(prep3);
+
+  var json_obj = {
+    response_number: b.response_number,
+    submit_date: b.submit_date,
+    dt_id: b.dt_id,
+    phone: b.phone,
+    dealership: b.dealership,
+    respondent_name: b.respondent_name,
+    good_communication: b.good_communication,
+    good_communication_score: b.good_communication_score,
+    satisfaction_with_install: b.satisfaction_with_install,
+    satisfaction_with_install_score: b.satisfaction_with_install_score,
+    satisfied_with_training: b.satisfied_with_training,
+    satisfied_with_training_score: b.satisfied_with_training_score,
+    recommend_implementation_team: b.recommend_implementation_team,
+    recommend_implementation_team_score: b.recommend_implementation_team_score,
+    expectations_set: b.expectations_set,
+    expectations_set_score: b.expectations_set_score,
+    my_expectations_met: b.my_expectations_met,
+    my_expectations_met_score: b.my_expectations_met_score,
+    recommend_dt: b.recommend_dt,
+    recommend_dt_score: b.recommend_dt_score,
+    decision_making_yn: b.decision_making_yn,
+    decision_making_yn_score: b.decision_making_yn_score,
+    sales_based_met_expectations: b.sales_based_met_expectations,
+    sales_based_met_expectations_score: b.sales_based_met_expectations_score,
+    sales_satisfaction: b.sales_satisfaction,
+    sales_satisfaction_score: b.sales_satisfaction_score,
+    sales_rep_satisfaction: b.sales_rep_satisfaction,
+    sales_rep_satisfaction_score: b.sales_rep_satisfaction_score,
+    part_of_install: b.part_of_install,
+    part_of_install_score: b.part_of_install_score,
+    ease_of_use: b.ease_of_use,
+    ease_of_use_score: b.ease_of_use_score,
+    dms_effectiveness: b.dms_effectiveness,
+    dms_effectiveness_score: b.dms_effectiveness_score,
+    compelled: b.compelled,
+    compelled_score: b.compelled_score,
+    primary_role: b.primary_role,
+    primary_role_id: b.primary_role_id,
+    comments: b.comments,
+    unsatisfied_comments: b.unsatisfied_comments
+  };
+
+  console.log(json_obj);
+  json_objs.push(json_obj);
+
+});
+
+json2csv({ data: json_objs, fields: fields }, function(err, csv) {
+  if (err) console.log(err);
+  fs.writeFile(filename, csv, function(err) {
+    if (err) throw err;
+    console.log('file saved');
+  });
+});
+
+// console.log(convert_to_json);
+
+module.exports = app;
